73fec7fe3c547dff593cc2d840a7160a39228aa2
Siyu Guo <enyacat@gmail.com>
2022-02-05 23:40:59 +1100

    add menu, food data and food grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components"
 import React from "react";
 import { Navbar } from "./Navbar/Navbar";
 import { Banner } from "./Banner/Banner";
+import { Menu } from "./Menu/Menu";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -21,6 +22,7 @@ function App() {
       <GlobalStyle />
       <Navbar />
       <Banner />
+      <Menu />
       <div>Hello Pizza2go</div>
     </>
   );
