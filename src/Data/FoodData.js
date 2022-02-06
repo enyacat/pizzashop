48ce21901b7a4aa802e300d106fadde664f56aed
Siyu Guo <enyacat@gmail.com>
2022-02-06 14:03:12 +1100

    add section and ingredients data and pictures

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,23 +1,272 @@
 export const foods = [
     {
         name: 'Garlic bread',
-        img: '/img/starters/Garlic_Bread.jpeg'
+        img: '/img/Starters/Garlic_Bread.jpeg',
+        section: 'Starters',
+        ingredients: ''
     },
     {
         name: 'Chicken wings',
-        img: '/img/starters/Chicken_wings.jpeg'
+        img: '/img/Starters/Chicken_wings.jpeg',
+        section: 'Starters',
+        ingredients: ''
     },
     {
         name: 'Garlic cheese pizza',
-        img: '/img/starters/Garlic_Cheese.jpeg'
+        img: '/img/Starters/Garlic_Cheese.jpeg',
+        section: 'Starters',
+        ingredients: ''
     },
     {
         name: 'Olive feta pizza',
-        img: '/img/starters/Olive_Feta.jpeg'
+        img: '/img/Starters/Olive_Feta.jpeg',
+        section: 'Starters',
+        ingredients: 'Garlic base, cheese, olive and feta'
     },
     {
         name: 'Wedges',
-        img: '/img/starters/Wedges.jpeg'
+        img: '/img/Starters/Wedges.jpeg',
+        section: 'Starters',
+        ingredients: 'With sour cream and sweet chilli sauce'
     },
-
+    {
+        name: 'Margarita',
+        img: '/img/traditional/Margarita.jpeg',
+        section: 'Traditional pizza',
+        ingredients: 'Napolitana Sauce, Cheese, Oregano'
+    },
+    {
+        name: 'Pepperoni',
+        img: '/img/traditional/Pepperoni.jpeg',
+        section: 'Traditional pizza',
+        ingredients: 'Hot Peperoni'
+    },
+    {
+        name: 'Hawaiian',
+        img: '/img/traditional/Hawaiian.jpeg',
+        section: 'Traditional pizza',
+        ingredients: 'Ham And Pineapple'
+    },
+    {
+        name: 'Oz',
+        img: '/img/traditional/Oz.jpeg',
+        section: 'Traditional pizza',
+        ingredients: 'Ham, Bacon, Egg'
+    },
+    {
+        name: 'Bolognese',
+        img: '/img/traditional/Bolognese.jpeg',
+        section: 'Traditional pizza',
+        ingredients: 'Tom And Garlic Mix Base , Beef Mince, Tomato, Onion, Cheese And Herbs'
+    },
+    {
+        name: 'Supreme',
+        img: '/img/gourmet/Supreme.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Ham Pepperoni, Mushroom, Onion, Capsicum, Pineapple, Cheese'
+    },
+    {
+        name: 'BBQ Chicken',
+        img: '/img/gourmet/BBQ_Chicken.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'BBQ Base, Chicken, Mushroom, Onion, Pineapple, Cheese'
+    },
+    {
+        name: 'Mexican',
+        img: '/img/gourmet/Mexican.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Pepperoni, Mushroom,Onion, Capsicum, Jalapenos, Cheese, Chilli Flakes'
+    },
+    {
+        name: 'Meat Lovers',
+        img: '/img/gourmet/Meat_Lovers.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'BBQ Base, Ham Pepperoni, Cabanossis, Bacon, Cheese'
+    },
+    {
+        name: 'Med Lamb',
+        img: '/img/gourmet/Med_Lamb.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Garlic Base, Lamb, Tomato, Onion, Capsicum, Cheese, Oregano (Topped With Mint Yoghurt And Lemon Wedge)'
+    },
+    {
+        name: 'Peri-Peri Chicken',
+        img: '/img/gourmet/Peri_Peri_Chicken.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Chicken , Caramelized Onion, Red Capsicum, Shallots, Cheese (Topped With Peri Peri Sauce)'
+    },
+    {
+        name: 'Vegetarian',
+        img: '/img/gourmet/Vegeterian.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Mushroom, Tomatoes, Red Capsicum, Onion, Pineapple, Cheese, Olives (Topped With Pesto Aioli)'
+    },
+    {
+        name: 'Alex',
+        img: '/img/gourmet/ALEX.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Tomato, Onion, Shallots, Cheese, Olive, Feta, Oregano'
+    },
+    {
+        name: 'Tandoori Chicken',
+        img: '/img/gourmet/Tandoori_Chicken.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Chicken, Mushroom, Onion And Capsicum On A Tandoori Sauce Base'
+    },
+    {
+        name: 'Med Vege',
+        img: '/img/gourmet/MED_Vege.jpeg',
+        section: 'Gourmet pizza',
+        ingredients: 'Tom Base, Zucchini, Red Capsicum, Eggplant, Cheese, Olive, Feta (Topped With Pesto Aioli)'
+    },
+    {
+        name: 'Garlic Prawns',
+        img: '/img/seafood/Garlic_Prawn.jpeg',
+        section: 'Seafood',
+        ingredients: 'Tom Base, Red Capsicum, Shallots, Sundried Tomatoes, Cheese, Garlic Marinated Prawns, Feta, (Served With Lemon Wedges)'
+    },
+    {
+        name: 'Szechuan Prawn',
+        img: '/img/seafood/Szechuan_Prawn.jpeg',
+        section: 'Seafood',
+        ingredients: 'Sweet-Chilli Sauce Base, Green Capsicum, Shallots, Cheese, Szechuan Seasoning Marinated Prawns(Served With Lemon Wedges)'
+    },
+    {
+        name: 'Hot Chilli Prawns',
+        img: '/img/seafood/Hot_Chilli_Prawns.jpeg',
+        section: 'Seafood',
+        ingredients: 'Garlic Base, Chilli Sauce Marinated Prawns, Cheese (Served With Fresh Shallots And Lemon Wedges)'
+    },
+    {
+        name: 'Vegetarian',
+        img: '/img/coming_soon.jpeg',
+        section: 'Pasta',
+        ingredients: 'Garlic, Mushroom, Onion, Capsicum, Olives, Eggplant, Tomato Sauce (Served With Parmesan-Cheese And Fresh Shallots)'
+    },
+    {
+        name: 'Bolognese',
+        img: '/img/coming_soon.jpeg',
+        section: 'Pasta',
+        ingredients: 'Beef Mince, Garlic Tomato (Served With Parmesan-Cheese And Fresh Shallots)'
+    },
+    {
+        name: 'Pollo e Funghi',
+        img: '/img/coming_soon.jpeg',
+        section: 'Pasta',
+        ingredients: 'Garlic, Chicken, Mushroom, Beef Cubes (Served With Parmesan-Cheese And Fresh Shallots)'
+    },
+    {
+        name: 'Boscaiola',
+        img: '/img/coming_soon.jpeg',
+        section: 'Pasta',
+        ingredients: 'Garlic, Mushroom, Ham, Bacon, Beef Cubes, Creamy Sauce(Served With Parmesan-Cheese And Fresh Shallots)'
+    },
+    {
+        name: 'Carbonara',
+        img: '/img/coming_soon.jpeg',
+        section: 'Pasta',
+        ingredients: 'Garlic, Bacon, Mushroom, Beef Cubes, Egg (Served With Parmesan-Cheese And Fresh Shallots)'
+    },
+    {
+        name: 'Nutella And Strawberry Pizza',
+        img: '/img/desserts/Nutella_Strawberry.jpeg',
+        section: 'Desserts',
+        ingredients: 'Nutella and Strawberry'
+    },
+    {
+        name: 'Nutella And Oreos Pizza',
+        img: '/img/coming_soon.jpeg',
+        section: 'Desserts',
+        ingredients: 'Nutella And Oreos'
+    },
+    {
+        name: 'Nutella And Strawberry And Oreo Pizza',
+        img: '/img/desserts/Nutella_Strawberry_Oreo.jpeg',
+        section: 'Desserts',
+        ingredients: ''
+    },
+    {
+        name: 'French Vanilla Cheesecake 450g',
+        img: '/img/coming_soon.jpeg',
+        section: 'Desserts',
+        ingredients: ''
+    },
+    {
+        name: 'Chocolate Bavarian 450g',
+        img: '/img/coming_soon.jpeg',
+        section: 'Desserts',
+        ingredients: ''
+    },
+    {
+        name: 'Strawberries And Cream 450g',
+        img: '/img/coming_soon.jpeg',
+        section: 'Desserts',
+        ingredients: ''
+    },
+    {
+        name: 'Cookies And Cream 450g',
+        img: '/img/coming_soon.jpeg',
+        section: 'Desserts',
+        ingredients: ''
+    },
+    {
+        name: 'Greek Salad',
+        img: '/img/coming_soon.jpeg',
+        section: 'Salads',
+        ingredients: 'Mixed Leaf, Tomatoes, Cucumber, Olives, Feta, Oregano, Balsamic Glaze'
+    },
+    {
+        name: '',
+        img: '/img/coming_soon.jpeg',
+        section: 'Salads',
+        ingredients: 'Mixed Leaf, Chicken, Bacon, Croutons, Caesar Dressing'
+    },
+    {
+        name: 'Garden Salad',
+        img: '/img/salads/Garden_salad.jpeg',
+        section: 'Salads',
+        ingredients: 'Mixed Leaf, Cucumber, Onion, Cherry Tomatoes, Vegetable Oil, Fresh Lemon Juice And Balsamic Glaze'
+    },
+    {
+        name: 'Deal One',
+        img: '/img/deals.jpg',
+        section: 'Deals',
+        ingredients: '2 Medium Pizzas, 2 Garlic Breads, 1 Bottle Soft Drink'
+    },
+    {
+        name: 'Deal Two',
+        img: '/img/deals.jpg',
+        section: 'Deals',
+        ingredients: '2 Large Pizzas, 2 Garlic Bread And One 1.25L Soft Drink'
+    },
+    {
+        name: 'Deal Three',
+        img: '/img/deals.jpg',
+        section: 'Deals',
+        ingredients: '3 Medium Pizzas, 2 Garlic Breads, 1 Bottle Soft Drink'
+    },
+    {
+        name: 'Deal Four',
+        img: '/img/deals.jpg',
+        section: 'Deals',
+        ingredients: '3 Large Pizzas, 2 Garlic Breads, 1 Bottle Soft Drink'
+    },
+    {
+        name: '1.25L Bottles Soft Drink',
+        img: '/img/drinks.png',
+        section: 'Beverages',
+        ingredients: ''
+    },
+    {
+        name: 'Can Soft Drink',
+        img: '/img/drinks.png',
+        section: 'Beverages',
+        ingredients: ''
+    },
+    {
+        name: '600ml Water',
+        img: '/img/water.png',
+        section: 'Beverages',
+        ingredients: ''
+    }
 ]
