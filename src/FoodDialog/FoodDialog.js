6fb97cec0b0eb5bd864a55e9deb110e28c29f4f8
Siyu Guo <enyacat@gmail.com>
2022-02-13 00:21:39 +1100

    Add price

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -2,7 +2,8 @@ import React from "react";
 import styled from 'styled-components'
 import { FoodLabel } from "../Menu/FoodGrid";
 import { pizzaRed } from "../Styles/colors";
-import { Title } from "../Styles/title"
+import { Title } from "../Styles/title";
+import { formatPrice } from "../Data/FoodData";
 
 
 const Dialog = styled.div`
@@ -69,7 +70,7 @@ export function FoodDialog({ openFood, setOpenFood, setOrders, orders }) {
     if (!openFood) return null;
 
     const order = {
-        name: openFood.name
+        ...openFood
     }
 
     function addToOrder() {
@@ -86,7 +87,7 @@ export function FoodDialog({ openFood, setOpenFood, setOrders, orders }) {
                 </DialogBanner>
                 <DialogContent></DialogContent>
                 <DialogFooter>
-                    <ConfirmButton onClick={addToOrder}>Add to order</ConfirmButton>
+                    <ConfirmButton onClick={addToOrder}>Add to order: {formatPrice(openFood.price)}</ConfirmButton>
                 </DialogFooter>
             </Dialog>
         </>
